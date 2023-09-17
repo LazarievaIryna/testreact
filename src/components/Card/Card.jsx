23e78d78ac634f7c778bb03deb9ddfd53597cc22
Iryna Lazareva <il337733@gmail.com>
2023-09-17 21:24:05 +0300

    change

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,25 +1,34 @@
-import css from './Card.module.css'
+import css from './Card.module.css';
+import { Button } from 'components/Button/Button';
 export const Card = ({ car}) =>{
+
+    const adress = car.address.split(', ');
+    const countrieCar = adress[adress.length - 1];
+    const cityCar = adress[adress.length - 2];
+    const func = car.functionalities[0];
+    console.log(func)
+
     return(<>
        <li className={css.card}>
         <img src={car.img} alt="car" className={css.img}/>
         <div>
-            <div>
-                <p>{car.make}</p><p>{car.model}</p>,{' '}
-                <p>{car.year}</p>
+            <div className={css.titleCard}>
+                <p>{car.make} <span className={css.titleAccent}>{car.model}</span>, {car.year}</p>
                 <p>{car.rentalPrice}</p>
             </div>
-            <div>
-                <ul>
-                    <li>{car.address}</li>
-                    <li>{car.rentalCompany}</li>
-                    <li>{car.type}</li>
-                    <li>{car.model}</li>
-                    <li>{car.mileage}</li>
-                    <li>{car.functionalities}</li>
+            <div className={css.wrapperDescription}>
+                <ul className={css.listCardescription}>
+                    <li className={css.text}>{countrieCar}</li>
+                    <li className={css.text}>{cityCar}</li>
+                    <li className={css.text}>{car.rentalCompany}</li>
+                    <li className={css.text}>{car.type}</li>
+                    <li className={css.text}>{car.model}</li>
+                    <li className={css.text}>{car.mileage}</li>
+                    <li className={css.textLast}>{car.functionalities[0]}</li>
                 </ul>
             </div>
         </div>
+        <Button/>
        </li>
        </>
 
